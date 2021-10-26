d527e8f4209c3b886f942ca7ce871898ad471312
narciSSuka <75092147+SergeyJoly1@users.noreply.github.com>
2021-10-26 22:18:39 +0300

    first commit

---
+++ FormEmail/WebView.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FormEmail
+{
+    public partial class WebView : Form
+    {
+        public WebView()
+        {
+            InitializeComponent();
+            WebBrowser webBrowser = new WebBrowser();
+            Controls.Add(webBrowser);
+            webBrowser.Size = Size;
+            webBrowser.Dock = DockStyle.Fill;
+            webBrowser.Navigate("ya.ru");
+            
+        }
+
+        private void WebView_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
